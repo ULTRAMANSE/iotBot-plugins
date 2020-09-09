9d8ad285a9702779b731f5fd4ef6ebc9be041dc3
ULTRAMANSE <1076012962@qq.com>
2020-09-09 21:03:13 +0800

    更新

--- plugins/bot_news_day.py
+++ plugins/bot_news_day.py
@@ -5,12 +5,16 @@ from iotbot.decorators import equal_content, not_botself
 from iotbot.sugar import Text
 
 @not_botself
-@equal_content('鸡汤')
+@equal_content('新闻')
 def receive_group_msg(ctx: GroupMsg):
 	try:
-		rep = requests.get('http://api.btstu.cn/yan/api.php?charset=utf-8&encode=json', timeout=10)
+		temp = []
+		rep = requests.get('http://api.tianapi.com/world/index?key=9ce8f79f3ad7ce68c6471bda7c4e2863&num=15', timeout=10)
 		rep.raise_for_status()
-		content: str = rep.json()['text']
-		Text(content)
+		for i in range(len(rep.json()['newslist'])):
+			temp.append(rep.json()['newslist'][i]['title'])
+			temp.append(rep.json()['newslist'][i]['url'])
+		max_len = max([len(x) for x in temp])
+		Text('\n'.join([x.center(max_len) for x in temp]))
 	except Exception as e:
 		print(e)
